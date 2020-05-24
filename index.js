f6e6d74d1072606f386a6b83072758d1b08cf81f
Ryan-Albuquerque <ryan.albuquerque2002@gmail.com>
2020-05-24 00:18:50 -0400

    fix 4

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ app.use(express.json())
 app.use(route)
 
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port port!`))
